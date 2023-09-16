e8ca98628079d6d2fb6eaef248c27b4394c846be
Yusupov Elbek <elbeky829@gmail.com>
2023-09-17 01:21:29 +0500

    header beginning added(bugs aviable)

---
+++ components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav>
+        <div className="container">
+            <div className="nav__wrapper">
+                <a className="nav__logo"><img src="assets/nav-logo.svg" alt="" /></a>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Header
